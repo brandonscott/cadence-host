bb3bcbc576bf3bec5c6d48cc2637c7fe5a0bc7c8
Brandon Scott <brandon.scott03@gmail.com>
2014-05-05 10:28:17 +0100

    Initial Commit

---
+++ CadenceHost.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CadenceHost.Tests
+{
+    [TestClass]
+    public class StatisticsTest
+    {
+        [TestMethod]
+        public void TestCPU()
+        {
+        }
+    }
+}
