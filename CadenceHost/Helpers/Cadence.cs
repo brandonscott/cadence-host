271cad7d6631570293395749b5e0436cdee903cf
Brandon Scott <brandon.scott03@gmail.com>
2014-05-08 15:30:40 +0100

    Added a Cadence helper to send requests.  Added Statistics helper to get information from Windows.

---
+++ CadenceHost/Helpers/Cadence.cs
@@ -0,0 +1,105 @@
+﻿// ---------------------------------------------------------------------------------------
+//  <copyright file="Cadence.cs" company="Cadence">
+//      Copyright © 2013-2014 by Brandon Scott and Christopher Franklin.
+// 
+//      Permission is hereby granted, free of charge, to any person obtaining a copy of
+//      this software and associated documentation files (the "Software"), to deal in
+//      the Software without restriction, including without limitation the rights to
+//      use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//      of the Software, and to permit persons to whom the Software is furnished to do
+//      so, subject to the following conditions:
+// 
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//      WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//      CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+//  </copyright>
+//  ---------------------------------------------------------------------------------------
+
+#region
+
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using PubNubMessaging.Core;
+
+#endregion
+
+namespace CadenceHost.Helpers
+{
+    /// <summary>
+    /// Sends and retrieves data from Cadence
+    /// </summary>
+    public static class Cadence
+    {
+        public static readonly String ServerUri = "http://cadence-bu.cloudapp.net/servers";
+        public static readonly String PulseUri = "http://cadence-bu.cloudapp.net/pulses";
+
+        public static readonly String Username = "brandon@brandonscott.co.uk";
+        public static readonly String Password = "Cadenc3!";
+
+        /// <summary>
+        /// Creates a Cadence server instance
+        /// </summary>
+        /// <param name="kvpData">The server data passed to Cadence</param>
+        public static int CreateServer(NameValueCollection kvpData)
+        {
+            return PostRequest(kvpData, ServerUri);
+        }
+
+        /// <summary>
+        /// Sends a pulse for the current server
+        /// </summary>
+        /// <param name="kvpData">The pulse data to pass to Cadence</param>
+        public static int SendPulse(NameValueCollection kvpData)
+        {
+            return PostRequest(kvpData, PulseUri);
+        }
+
+        /// <summary>
+        /// Updates the current Cadence server instance
+        /// </summary>
+        /// <param name="kvpData">The server data to update for the current server</param>
+        public static void UpdateServer(NameValueCollection kvpData)
+        {
+            PutRequest(kvpData);
+        }
+
+        /// <summary>
+        /// The base Put web request to send to the server
+        /// </summary>
+        /// <param name="kvpData">The data to send to the server</param>
+        private static void PutRequest(NameValueCollection kvpData)
+        {
+            using (var wb = new WebClient())
+            {
+                var response = wb.UploadValues(ServerUri, "PUT", kvpData);
+            }
+        }
+
+        /// <summary>
+        /// The base Post web request to send to the server
+        /// </summary>
+        /// <param name="kvpData">The data to send to the server</param>
+        /// <param name="uri">The URI to send the request to</param>
+        private static int PostRequest(NameValueCollection kvpData, String uri)
+        {
+            var nc = new NetworkCredential("brandon@brandonscott.co.uk", "Cadenc3!");
+            string responseJson;
+            using (var wb = new WebClient {Credentials = nc})
+            {
+                var response = wb.UploadValues(uri, "POST", kvpData);
+                responseJson = wb.Encoding.GetString(response);
+            }
+            return JObject.Parse(responseJson).GetValue("id").Value<int>();
+        }
+    }
+}
--- CadenceHost/Helpers/Statistics.cs
+++ CadenceHost/Helpers/Statistics.cs
@@ -1,16 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// ---------------------------------------------------------------------------------------
+//  <copyright file="Statistics.cs" company="Cadence">
+//      Copyright © 2013-2014 by Brandon Scott and Christopher Franklin.
+// 
+//      Permission is hereby granted, free of charge, to any person obtaining a copy of
+//      this software and associated documentation files (the "Software"), to deal in
+//      the Software without restriction, including without limitation the rights to
+//      use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//      of the Software, and to permit persons to whom the Software is furnished to do
+//      so, subject to the following conditions:
+// 
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//      WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//      CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+//  </copyright>
+//  ---------------------------------------------------------------------------------------
+
+#region
+
+using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
+
+#endregion
 
 namespace CadenceHost.Helpers
 {
     public class Statistics
     {
-        readonly PerformanceCounter _cpuCounter;
-        readonly PerformanceCounter _ramCounter;
+        private readonly PerformanceCounter _cpuCounter;
+        private readonly PerformanceCounter _ramCounter;
+        private readonly PerformanceCounter _totalRamCounter;
 
         public Statistics()
         {
@@ -22,6 +51,7 @@ namespace CadenceHost.Helpers
             };
 
             _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+          
             //Set up CPU Counter for the first run
             _cpuCounter.NextValue();
         }
@@ -32,7 +62,7 @@ namespace CadenceHost.Helpers
         /// <returns>The current CPU usage</returns>
         public string GetCurrentCpu()
         {
-            return _cpuCounter.NextValue() + "%";
+            return _cpuCounter.NextValue().ToString(CultureInfo.InvariantCulture);
         }
 
         /// <summary>
@@ -41,7 +71,40 @@ namespace CadenceHost.Helpers
         /// <returns>The current RAM usage</returns>
         public string GetCurrentRam()
         {
-            return _ramCounter.NextValue() + "MB";
-        } 
+            return _ramCounter.NextValue().ToString(CultureInfo.InvariantCulture);
+        }
+
+        public String GetTotalRamSize()
+        {
+            var mc = new ManagementClass("Win32_ComputerSystem");
+            var moc = mc.GetInstances();
+            foreach (var item in moc.Cast<ManagementObject>())
+            {
+                return Convert.ToString(Math.Round(Convert.ToDouble(item.Properties["TotalPhysicalMemory"].Value) / 1048576, 2));
+            }
+            return "RAMsize";
+        }
+
+        public String GetOsName()
+        {
+            var name = (from x in new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem").Get().OfType<ManagementObject>()
+                        select x.GetPropertyValue("Caption")).FirstOrDefault();
+            return name != null ? name.ToString() : "Unknown";
+        }
+
+        public String GetOsVersion()
+        {
+            var name = (from x in new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem").Get().OfType<ManagementObject>()
+                        select x.GetPropertyValue("Version")).FirstOrDefault();
+            return name != null ? name.ToString() : "Unknown";
+        }
+
+        public String GetTotalDiskStorage()
+        {
+            var driveInfo = new DriveInfo(@"C");
+            var freeSpaceAsMb = driveInfo.TotalSize / 1048576;
+            return freeSpaceAsMb.ToString(CultureInfo.InvariantCulture);
+        }
+
     }
 }
